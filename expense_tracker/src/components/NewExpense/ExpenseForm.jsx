6602cc3766f06e24a3fc55f8a169293f8a6ad86a
Sushil Maurya <104303944+Sushil-171M@users.noreply.github.com>
2024-03-28 23:01:09 +0530

    Added EventHandler to each Input field
    Added eventHandler to each Input Field
    and stored data using useState.

--- expense_tracker/src/components/NewExpense/ExpenseForm.jsx
+++ expense_tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 
 const ExpenseForm = () => {
+
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState(0)
+    const [date, setDate] = useState(null)
+
+    const titleHandler = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const amountHandler = (e) => {
+        setAmount(e.target.value)
+    }
+    const dateHandler = (e) => {
+        setDate(e.target.value)
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Expense Amount</label>
-                    <input type="number" />
+                    <input type="number" onChange={amountHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" />
+                    <input type="date" onChange={dateHandler} />
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">Add Expense</button>
